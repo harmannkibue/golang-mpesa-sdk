f88ef7dd93a9d302d323911dc1b76330785dd2a5
harmannkibue <harmannkibue@gmail.com>
2023-05-23 14:17:10 +0300

    add api key and api secrets for daraja apis

--- examples/main.go
+++ examples/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/harmannkibue/golang-mpesa-sdk/internal/utils/httprequest"
 	"os"
 )
 
@@ -12,19 +11,19 @@ var (
 )
 
 func main() {
-	fmt.Println("Heere in the main function!")
+	fmt.Printf("Safaricom: %s AND %s ", os.Getenv("MPESA_KEY"), os.Getenv("MPESA_SECRET"))
 
-	httpReq := httprequest.HttpRequest{}
+	//httpReq := httprequest.HttpRequest{}
+	//
+	//resp, err := httpReq.PerformPost(httprequest.RequestDataParams{
+	//	Endpoint:    "https://webhook.site/93e45dd0-d2ce-42e9-8f90-a55767a4e982",
+	//	ContentType: "application/json",
+	//	Data:        []byte(`{"key": "value"}`),
+	//})
+	//
+	//if err != nil {
+	//	return
+	//}
 
-	resp, err := httpReq.PerformPost(httprequest.RequestDataParams{
-		Endpoint:    "https://webhook.site/93e45dd0-d2ce-42e9-8f90-a55767a4e982",
-		ContentType: "application/json",
-		Data:        []byte(`{"key": "value"}`),
-	})
-
-	if err != nil {
-		return
-	}
-
-	fmt.Println("THE RESPONSE ISS ", resp.StatusCode)
+	//fmt.Println("THE RESPONSE ISS ", resp.StatusCode)
 }
