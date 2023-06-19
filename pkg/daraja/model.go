0841f9085b1976a9e3e2ad41961c6f08c4806bdf
harmannkibue <harmannkibue@gmail.com>
2023-06-19 17:22:48 +0300

    Add more loging on failed post requests

--- pkg/daraja/model.go
+++ pkg/daraja/model.go
@@ -55,3 +55,38 @@ type C2BSimulateResponse struct {
 	ResponseCode            string `json:"ResponseCode"`
 	ResponseDescription     string `json:"ResponseDescription"`
 }
+
+// B2CRequestBody -.
+type B2CRequestBody struct {
+	InitiatorName      string `json:"InitiatorName"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	Amount             int    `json:"Amount"`
+	PartyA             int    `json:"PartyA"`
+	PartyB             int64  `json:"PartyB"`
+	Remarks            string `json:"Remarks"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	ResultURL          string `json:"ResultURL"`
+	Occassion          string `json:"Occassion"`
+}
+
+// B2CResponseBody -.
+type B2CResponseBody struct {
+	ConversationID           string `json:"ConversationID"`
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+//{
+//"InitiatorName": "testapi",
+//"SecurityCredential": "YL2cUjka1r1YSZsI/9ATatEtK8DruJwyx/leMHV0kr+bUTO6UciatgwkLuVYrHEWScIHYIlomBxXbm3813aVe4mpoUUU2/EAHJPcdbBQTRat1rcuyudv3TYkBR28mA+M+Y+NiyeLLGQkSxLUBPga5xk7z4tjgERoAPnsfmw/NijdS3ArZKZ+WZWPU+WMBvDU5ZouWJVPMzJ99/MCY0LY5TNUNKwYvlxxgjTgpUl4+5k1dQW8VEM61fxUl4aeJx8WKXOZt4bf+XmBLkdVwCaRyacyYoaK89sT53EadfNUU/whytJYcLr7rOHSQDxc5QqkbE4ekhklzjpuGk6ZRhkTTQ==",
+//"CommandID": "BusinessPayment",
+//"Amount": 1,
+//"PartyA": 600426,
+//"PartyB": 254708374149,
+//"Remarks": "Testing B2C",
+//"QueueTimeOutURL": "https://webhook.site/c882c5f6-4209-4f12-911b-85f13a69eb65",
+//"ResultURL": "https://webhook.site/c882c5f6-4209-4f12-911b-85f13a69eb65",
+//"Occassion": "Testing B2C"
+//}
